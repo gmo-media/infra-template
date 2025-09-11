d623ee4896ffc23e04647b97e701ccec5753115b
motoki317 <motoki317@gmail.com>
2025-09-11 15:21:05 +0900

    fix vpc peering tables

--- dev/vpc_peering.tf
+++ dev/vpc_peering.tf
@@ -3,24 +3,34 @@
 #   id = "vpc-abcdef"
 # }
 #
+# locals {
+#   other-route-table-ids = [
+#     # Describe your route table IDs here
+#     "rtb-abcdef",
+#   ]
+# }
+#
 # resource "aws_vpc_peering_connection" "peering" {
 #   vpc_id      = module.vpc.vpc_id
 #   peer_vpc_id = data.aws_vpc.other.id
 #   auto_accept = true
 # }
 #
-# resource "aws_route" "private-peering" {
-#   for_each = toset(module.vpc.private_route_table_ids)
+# resource "aws_route" "peering" {
+#   for_each = toset(concat(
+#     module.vpc.private_route_table_ids,
+#     module.vpc.public_route_table_ids,
+#   ))
 #
 #   route_table_id            = each.key
 #   destination_cidr_block    = data.aws_vpc.other.cidr_block
 #   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 # }
 #
-# resource "aws_route" "public-peering" {
-#   for_each = toset(module.vpc.public_route_table_ids)
+# resource "aws_route" "peering-back" {
+#   for_each = toset(local.other-route-table-ids)
 #
 #   route_table_id            = each.key
-#   destination_cidr_block    = data.aws_vpc.other.cidr_block
+#   destination_cidr_block    = module.vpc.vpc_cidr_block
 #   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 # }

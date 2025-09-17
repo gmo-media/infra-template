0a06b8b67a4d330825aa634c1c329dfa46777a6d
motoki317 <motoki317@gmail.com>
2025-09-17 10:11:38 +0900

    fix: Pin karpenter module

--- dev/eks_karpenter.tf
+++ dev/eks_karpenter.tf
@@ -1,5 +1,6 @@
 module "karpenter" {
-  source = "terraform-aws-modules/eks/aws//modules/karpenter"
+  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version = "21.3.0"
 
   cluster_name = module.eks.cluster_name
   namespace    = "karpenter"

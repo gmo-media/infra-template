f344c0d526903492a30ed9cd966084019e8a9bee
motoki317 <motoki317@gmail.com>
2025-10-02 11:38:47 +0900

    feat!: Update tofu-actions to v5

--- .github/tofu-actions-config.js
+++ .github/tofu-actions-config.js
@@ -1,6 +1,19 @@
+// See https://github.com/gmo-media/tofu-actions for syntax.
+
+const commonConfig = {
+  tfBinary: 'tofu',
+  auth: {
+    mode: 'aws-oidc',
+    awsRegion: 'ap-northeast-1',
+    awsPlanRole: 'arn:aws:iam::<aws-account-id>:role/tofu-plan',
+    awsApplyRole: 'arn:aws:iam::<aws-account-id>:role/tofu-apply',
+  },
+};
+
+
 export default {
-  dirs: [
-    'common',
-    'dev',
-  ]
+  dirs: {
+    'common': commonConfig,
+    'dev': commonConfig,
+  }
 }
